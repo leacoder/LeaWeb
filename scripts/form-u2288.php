5db679945c9efa20fa2fe3bda13fbd86d328463d
Leandro <leandrogabrielgarcia@gmail.com>
2014-10-07 22:45:27 -0300

    Lista La version Mobile y web
    Revisar el "contacto" de la version mobile.

--- scripts/form-u2288.php
+++ scripts/form-u2288.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
 function emailFormSubmission()
 {
-	$to = 'office@onscreen3d.com';
+	$to = 'lgdesign@lgdesign.t15.org';
 	$subject = 'Feedback Form Submission';
 	
 	$message = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/><title>' . htmlentities($subject,ENT_COMPAT,'UTF-8') . '</title></head>';
